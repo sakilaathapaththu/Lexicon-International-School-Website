e3b04cf1e653bc493605659bf6b20e936586c75b
Thamindu Sulakshana <89307986+ThaminduSulakshana@users.noreply.github.com>
2025-07-07 23:07:11 +0530

    home.blade.php

---
+++ Lexicon/resources/views/home/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home - Test Laravel</title>
+</head>
+<body>
+    <h1>Welcome to Home Page!</h1>
+    <p>This is a simple Blade view working with Laravel.</p>
+</body>
+</html>
