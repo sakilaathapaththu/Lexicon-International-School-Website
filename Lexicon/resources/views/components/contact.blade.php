7c98431233e3f5eb541f9a255717184de1db3551
Praveen Liyanage <111709030+PraveenLiyanage@users.noreply.github.com>
2025-07-13 03:50:41 +0530

    Added CSRF Token for contact.blade.php

--- Lexicon/resources/views/components/contact.blade.php
+++ Lexicon/resources/views/components/contact.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 <head>
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us - Lexicon International School</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
